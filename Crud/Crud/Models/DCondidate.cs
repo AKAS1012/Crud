d102c31a079f5720259270127d6c1b7c213009d9
Akash kumar <atulaksh786@gmail.com>
2022-11-02 22:28:39 +0530

    initial commit

---
+++ Crud/Crud/Models/DCondidate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud.Models
+{
+    public class DCondidate
+    {
+        [Key]
+        public int id { get; set; }
+        [Column(TypeName="nvarchar(100)")]
+        public string fullname { get; set; }
+        [Column(TypeName = "nvarchar(16)")]
+        public string mobile { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string email { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string age { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string bloodgroup { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string address { get; set; }
+    }
+}
---
+++ Crud/Crud/Models/DonationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Models
+{
+    public class DonationDBContext:DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext>options):base(options)
+        {
+
+        }
+        public DbSet<DCondidate> DCondidates { get; set; }
+    }
+}
